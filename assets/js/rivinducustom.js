0cbbcfb32158f1c8b9f0aaef59c13c9f941ca189
Rivindu Perera <rivinduperera@users.noreply.github.com>
2015-06-07 01:48:40 +1200

    Website update

---
+++ assets/js/rivinducustom.js
@@ -0,0 +1,35 @@
+function showAllPubs() 
+{ 
+	var divsToShow = document.getElementsByClassName('allpub'),i;
+
+    for(var i = 0; i < divsToShow.length; i++)
+    {
+		divsToShow[i].style.display = 'block';
+    }
+}
+
+function showOnlyDissertations()
+{
+	showAllPubs();
+	var divsToHide = document.getElementsByClassName('conferencepub'),i;
+
+    for(var i = 0; i < divsToHide.length; i++)
+    {
+		divsToHide[i].style.display = 'none';
+    }
+	
+	
+}
+
+function showOnlyConferencePapers()
+{
+	showAllPubs();
+	
+	var divsToHide = document.getElementsByClassName("dissertationpub");
+
+    for(var i = 0; i < divsToHide.length; i++)
+    {
+		divsToHide[i].style.display = 'none';
+    }
+	
+}
