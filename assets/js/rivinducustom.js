d825e305f768af1ee64c23f13a69a2730da1bf96
Rivindu Perera <rivinduperera@users.noreply.github.com>
2015-10-23 21:56:19 +1300

    Website updated

--- assets/js/rivinducustom.js
+++ assets/js/rivinducustom.js
@@ -8,8 +8,19 @@ function showAllPubs()
     }
 }
 
+function hideAllPubs()
+{
+    var divsToShow = document.getElementsByClassName('allpub'),i;
+
+    for(var i = 0; i < divsToShow.length; i++)
+    {
+		divsToShow[i].style.display = 'none';
+    }
+}
+
 function showOnlyDissertations()
 {
+    /*
 	showAllPubs();
 	var divsToHide = document.getElementsByClassName('conferencepub'),i;
 
@@ -17,12 +28,23 @@ function showOnlyDissertations()
     {
 		divsToHide[i].style.display = 'none';
     }
+    */
+    
+    hideAllPubs();
+    
+    var divsToHide = document.getElementsByClassName("dissertationpub");
+
+    for(var i = 0; i < divsToHide.length; i++)
+    {
+		divsToHide[i].style.display = 'block';
+    }
 	
 	
 }
 
 function showOnlyConferencePapers()
 {
+    /*
 	showAllPubs();
 	
 	var divsToHide = document.getElementsByClassName("dissertationpub");
@@ -31,5 +53,22 @@ function showOnlyConferencePapers()
     {
 		divsToHide[i].style.display = 'none';
     }
-	
+	*/
+    
+    hideAllPubs();
+    
+    var divsToHide = document.getElementsByClassName("conferencepub");
+
+    for(var i = 0; i < divsToHide.length; i++)
+    {
+		divsToHide[i].style.display = 'block';
+    }
+    
+}
+
+function filterByYear()
+{
+    showAllPubs();
+    
+    
 }
